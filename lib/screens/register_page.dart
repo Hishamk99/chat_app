d8a51cb63431ad8b2608857a20f7ff92825e927e
Hishamk99 <hishamkamel370@gmail.com>
2023-10-09 20:09:35 +0200

    refactor code

--- lib/screens/register_page.dart
+++ lib/screens/register_page.dart
@@ -76,23 +76,17 @@ class RegisterPage extends StatelessWidget {
             CustomButton(
               onTap: () async {
                 try {
-                  var auth = FirebaseAuth.instance;
-                  UserCredential userCredential =
-                      await auth.createUserWithEmailAndPassword(
-                          email: email!, password: password!);
+                  await registerUser();
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'weak-password') {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      content: Text('The password provided is too weak.'),
-                    ));
+                    showSnackBar(context, 'weak password');
                   } else if (e.code == 'email-already-in-use') {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text(
-                            'The account already exists for that email.')));
+                    showSnackBar(
+                        context, 'The account already exists for that email.');
                   }
                 }
-                ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('email already created')));
+                showSnackBar(
+                        context, 'Success');
               },
               text: 'REGISTER',
             ),
@@ -125,5 +119,18 @@ class RegisterPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void showSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+    ));
+  }
+
+  Future<UserCredential> registerUser() async {
+    var auth = FirebaseAuth.instance;
+    UserCredential userCredential = await registerUser();
+    return auth.createUserWithEmailAndPassword(
+        email: email!, password: password!);
   }
 }
