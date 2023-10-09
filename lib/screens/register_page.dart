fa06d48a3eca725f258238a7b492bc2a2f423345
Hishamk99 <hishamkamel370@gmail.com>
2023-10-09 21:09:26 +0200

    using ModalProgressHUD widget

--- lib/screens/register_page.dart
+++ lib/screens/register_page.dart
@@ -3,128 +3,143 @@ import 'package:chat_app/widgets/custom_button.dart';
 import 'package:chat_app/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   RegisterPage({super.key});
   static String id = 'RegisterPage';
 
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
   String? email;
+
   String? password;
+
+  bool isLoading = false;
+
   GlobalKey<FormState> formKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kPrimaryColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        child: Form(
-          key: formKey,
-          child: ListView(
-            children: [
-              const SizedBox(
-                height: 75,
-              ),
-              Image.asset(
-                'assets/images/scholar.png',
-                height: 100,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Scolar Chat',
-                    style: TextStyle(
-                      fontFamily: 'Pacifico',
-                      fontSize: 30,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 75,
-              ),
-              const Row(
-                children: [
-                  Text(
-                    'REGISTER',
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomTextField(
-                onChanged: (data) {
-                  email = data;
-                },
-                hint: 'Email',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              CustomTextField(
-                onChanged: (data) {
-                  password = data;
-                },
-                hint: 'Password',
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomButton(
-                onTap: () async {
-                  if (formKey.currentState!.validate()) {
-                    try {
-                      await registerUser();
-                    } on FirebaseAuthException catch (e) {
-                      if (e.code == 'weak-password') {
-                        showSnackBar(context, 'weak password');
-                      } else if (e.code == 'email-already-in-use') {
-                        showSnackBar(context,
-                            'The account already exists for that email.');
-                      }
-                    } catch (e) {
-                      showSnackBar(context, 'Error');
-                    }
-                    showSnackBar(context, 'Success');
-                  }
-                  else{
-                    
-                  }
-                },
-                text: 'REGISTER',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'already have an account ? ',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text(
-                      'Log In',
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: kPrimaryColor,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Form(
+            key: formKey,
+            child: ListView(
+              children: [
+                const SizedBox(
+                  height: 75,
+                ),
+                Image.asset(
+                  'assets/images/scholar.png',
+                  height: 100,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Scolar Chat',
                       style: TextStyle(
-                        color: Color(0XFFC7EDE6),
+                        fontFamily: 'Pacifico',
+                        fontSize: 30,
+                        color: Colors.white,
                       ),
                     ),
-                  )
-                ],
-              ),
-            ],
+                  ],
+                ),
+                const SizedBox(
+                  height: 75,
+                ),
+                const Row(
+                  children: [
+                    Text(
+                      'REGISTER',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomTextField(
+                  onChanged: (data) {
+                    email = data;
+                  },
+                  hint: 'Email',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  onChanged: (data) {
+                    password = data;
+                  },
+                  hint: 'Password',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomButton(
+                  onTap: () async {
+                    if (formKey.currentState!.validate()) {
+                      isLoading = true;
+                      setState((){});
+                      try {
+                        await registerUser();
+                        showSnackBar(context, 'Success');
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'weak-password') {
+                          showSnackBar(context, 'weak password');
+                        } else if (e.code == 'email-already-in-use') {
+                          showSnackBar(context,
+                              'The account already exists for that email.');
+                        }
+                      } catch (e) {
+                        showSnackBar(context, 'Error');
+                      }
+                      isLoading = false;
+                      setState((){});
+                    } else {}
+                  },
+                  text: 'REGISTER',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'already have an account ? ',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        'Log In',
+                        style: TextStyle(
+                          color: Color(0XFFC7EDE6),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
