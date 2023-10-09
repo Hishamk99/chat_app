2b94c041a56b95fa18cd7ad0b48ef7d8e531cfd9
Hishamk99 <hishamkamel370@gmail.com>
2023-10-09 17:43:51 +0200

    Initialization firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:chat_app/screens/login_page.dart';
 import 'package:chat_app/screens/register_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ChatApp());
 }
 
